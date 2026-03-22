0cbb0bc3b87b9b4cde16e50fd8804a376a98fa9b
amran-beep <andykantin1@gmail.com>
2026-03-23 05:20:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -201,6 +201,6 @@ Buat:
 // ================= PORT =================
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server jalan di port", PORT);
 });
